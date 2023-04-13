17691d264bd446225b4f43fc6813d025a79a09e5
mohamedInfath <mohamedinfath99@gmail.com>
2023-04-13 16:58:28 +0530

    two more images added in the header

--- main.py
+++ main.py
@@ -9,14 +9,32 @@ class Face_Recognition_System:
         self.root.geometry("1530x790+0+0")
         self.root.title("Face Recognition System")
 
+
+        # First Image
         img=Image.open(r"D:\05. ICBT Notes\Student-Attendance-System\college_images\IcbtLogo.jpg")
-        img=img.resize((500,130),Image.ANTIALIAS)
+        img=img.resize((500,150),Image.ANTIALIAS)
         self.photoimg=ImageTk.PhotoImage(img)
 
         f_lbl=Label(self.root,image=self.photoimg)
-        f_lbl.place(x=0, y=0, width=500, height=130)
+        f_lbl.place(x=0, y=0, width=500, height=150)
 
-a
+        # Second Image
+        img1=Image.open(r"D:\05. ICBT Notes\Student-Attendance-System\college_images\facialrecognition.png")
+        img1=img1.resize((500,150),Image.ANTIALIAS)
+        self.photoimg1=ImageTk.PhotoImage(img1)
+
+        f_lbl=Label(self.root,image=self.photoimg1)
+        f_lbl.place(x=500, y=0, width=500, height=150)
+
+        # Third Image 
+        img2=Image.open(r"D:\05. ICBT Notes\Student-Attendance-System\college_images\cardiff.jpg")
+        img2=img2.resize((500,150),Image.ANTIALIAS)
+        self.photoimg2=ImageTk.PhotoImage(img2)
+
+        f_lbl=Label(self.root,image=self.photoimg2)
+        f_lbl.place(x=1000, y=0, width=500, height=150)
+
+
 
 
 
