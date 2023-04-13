d1c7095425206122f15a41b8ae11bfaf71152bb5
mohamedInfath <mohamedinfath99@gmail.com>
2023-04-13 22:34:33 +0530

    main page code done

--- main.py
+++ main.py
@@ -46,6 +46,105 @@ class Face_Recognition_System:
         title_lbl=Label(bg_img,text="ICBT CAMPUS - STUDENT ATTENDENCE SYSTEM",font=("times new roman",28,"bold"),bg="grey",fg="black")
         title_lbl.place(x=0,y=0,width=1530,height=50)
 
+        # Student Button
+        img4=Image.open(r"D:\05. ICBT Notes\Student-Attendance-System\college_images\std.jpg")
+        img4=img4.resize((220,220),Image.ANTIALIAS)
+        self.photoimg4=ImageTk.PhotoImage(img4)
+
+        b1=Button(bg_img,image=self.photoimg4,cursor="hand2")
+        b1.place(x=200,y=100,width=220,height=220)
+
+        b1_1=Button(bg_img,text="STUDENT DETAILS",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=200,y=300,width=220,height=40)
+
+
+        # Detect Face Button
+        img5=Image.open(r"D:\05. ICBT Notes\Student-Attendance-System\college_images\fad.jpg")
+        img5=img5.resize((220,220),Image.ANTIALIAS)
+        self.photoimg5=ImageTk.PhotoImage(img5)
+
+        b1=Button(bg_img,image=self.photoimg5,cursor="hand2")
+        b1.place(x=500,y=100,width=220,height=220)
+
+        b1_1=Button(bg_img,text="FACE DETECTOR",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=500,y=300,width=220,height=40)
+
+
+        # Attendence Button
+        img6=Image.open(r"D:\05. ICBT Notes\Student-Attendance-System\college_images\attendant.jpg")
+        img6=img6.resize((220,220),Image.ANTIALIAS)
+        self.photoimg6=ImageTk.PhotoImage(img6)
+
+        b1=Button(bg_img,image=self.photoimg6,cursor="hand2")
+        b1.place(x=800,y=100,width=220,height=220)
+
+        b1_1=Button(bg_img,text="ATTENDENCE",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=800,y=300,width=220,height=40)
+
+
+        # Register New Admin Button
+        img7=Image.open(r"D:\05. ICBT Notes\Student-Attendance-System\college_images\employee.jpg")
+        img7=img7.resize((220,220),Image.ANTIALIAS)
+        self.photoimg7=ImageTk.PhotoImage(img7)
+
+        b1=Button(bg_img,image=self.photoimg7,cursor="hand2")
+        b1.place(x=1100,y=100,width=220,height=220)
+
+        b1_1=Button(bg_img,text="REGISTER ADMIN",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=1100,y=300,width=220,height=40)
+
+
+        # Train Data Button
+        img8=Image.open(r"D:\05. ICBT Notes\Student-Attendance-System\college_images\traindata.jpg")
+        img8=img8.resize((220,220),Image.ANTIALIAS)
+        self.photoimg8=ImageTk.PhotoImage(img8)
+
+        b1=Button(bg_img,image=self.photoimg8,cursor="hand2")
+        b1.place(x=200,y=380,width=220,height=220)
+
+        b1_1=Button(bg_img,text="TRAIN DATA",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=200,y=580,width=220,height=40)
+
+
+
+        # Photos Button
+        img9=Image.open(r"D:\05. ICBT Notes\Student-Attendance-System\college_images\photos.jpeg")
+        img9=img9.resize((220,220),Image.ANTIALIAS)
+        self.photoimg9=ImageTk.PhotoImage(img9)
+
+        b1=Button(bg_img,image=self.photoimg9,cursor="hand2")
+        b1.place(x=500,y=380,width=220,height=220)
+
+        b1_1=Button(bg_img,text="PHOTOS",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=500,y=580,width=220,height=40)
+
+
+        # Developer Button
+        img10=Image.open(r"D:\05. ICBT Notes\Student-Attendance-System\college_images\developer.jpg")
+        img10=img10.resize((220,220),Image.ANTIALIAS)
+        self.photoimg10=ImageTk.PhotoImage(img10)
+
+        b1=Button(bg_img,image=self.photoimg10,cursor="hand2")
+        b1.place(x=800,y=380,width=220,height=220)
+
+        b1_1=Button(bg_img,text="DEVELOPER",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=800,y=580,width=220,height=40)
+
+
+        # Exit Button
+        img11=Image.open(r"D:\05. ICBT Notes\Student-Attendance-System\college_images\exit.jpg")
+        img11=img11.resize((220,220),Image.ANTIALIAS)
+        self.photoimg11=ImageTk.PhotoImage(img11)
+
+        b1=Button(bg_img,image=self.photoimg11,cursor="hand2")
+        b1.place(x=1100,y=380,width=220,height=220)
+
+        b1_1=Button(bg_img,text="EXIT",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1.place(x=1100,y=580,width=220,height=40)
+
+
+
+
 
 
 
