774ca676564dcb735ff63a7e7e6dbd558f1ed33c
mohamedInfath <mohamedinfath99@gmail.com>
2023-04-14 21:17:45 +0530

    Right, Left frame seted

--- student.py
+++ student.py
@@ -44,8 +44,21 @@ class Student:
         bg_img.place(x=0, y=150, width=1530, height=710)
 
         title_lbl=Label(bg_img,text="STUDENT MANAGEMENT SYSTEM",font=("times new roman",28,"bold"),bg="grey",fg="black")
-        title_lbl.place(x=0,y=0,width=1530,height=50)
+        title_lbl.place(x=0,y=0,width=1530,height=45)
 
+
+        main_frame=Frame(bg_img,bd=2,bg="white")
+        main_frame.place(x=10,y=50,width=1500,height=575)
+
+
+        # Left Label Frame
+        Left_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="STUDENT DETAILS", font=("times new roman",12,"bold"))
+        Left_frame.place(x=10,y=10,width=760,height=560)
+
+
+        # Right Label Frame
+        Right_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="STUDENT DETAILS", font=("times new roman",12,"bold"))
+        Right_frame.place(x=790,y=10,width=690,height=560)
 
 
 
