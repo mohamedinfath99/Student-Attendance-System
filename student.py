c35aeb4a8e654f297a1720183895fe1df017446e
mohamedInfath <mohamedinfath99@gmail.com>
2023-04-15 00:42:04 +0530

    current course information part done

--- student.py
+++ student.py
@@ -55,10 +55,65 @@ class Student:
         Left_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="STUDENT DETAILS", font=("times new roman",12,"bold"))
         Left_frame.place(x=10,y=10,width=760,height=560)
 
+        img_left=Image.open(r"D:\05. ICBT Notes\Student-Attendance-System\college_images\student.jpg")
+        img_left=img_left.resize((720,150),Image.ANTIALIAS)
+        self.photoimg_left=ImageTk.PhotoImage(img_left)
+
+        f_lbl=Label(Left_frame,image=self.photoimg_left)
+        f_lbl.place(x=15, y=3, width=720, height=130)
+
+
+        # Current Course
+        current_course_frame=LabelFrame(Left_frame,bd=2,bg="white",relief=RIDGE,text="CURRENT COURSE INFORMATION", font=("times new roman",12,"bold"))
+        current_course_frame.place(x=5,y=135,width=740,height=150)
+
+
+        # Department
+        dep_label=Label(current_course_frame,text="Department",font=("times new roman",13,"bold"),bg="white")
+        dep_label.grid(row=0,column=0,padx=10,sticky=W)
+
+        dep_combo=ttk.Combobox(current_course_frame,font=("times new roman",13,"bold"),state="readonly",width=20)
+        dep_combo["values"]=("Select Department","Computer","Civil","Management","Auto Mobile")
+        dep_combo.current(0)
+        dep_combo.grid(row=0,column=1,padx=2,pady=10,sticky=W)
+
+
+        # Course
+        course_label=Label(current_course_frame,text="Course",font=("times new roman",13,"bold"),bg="white")
+        course_label.grid(row=0,column=2,padx=10,sticky=W)
+
+        course_combo=ttk.Combobox(current_course_frame,font=("times new roman",13,"bold"),state="readonly",width=20)
+        course_combo["values"]=("Select Course","SE","CE","BM","ME")
+        course_combo.current(0)
+        course_combo.grid(row=0,column=3,padx=2,pady=10,sticky=W)
+
+
+        # Year
+        year_label=Label(current_course_frame,text="Year",font=("times new roman",13,"bold"),bg="white")
+        year_label.grid(row=1,column=0,padx=10,sticky=W)
+
+        year_combo=ttk.Combobox(current_course_frame,font=("times new roman",13,"bold"),state="readonly",width=20)
+        year_combo["values"]=("Select Year","2020-21","2021-22","2022-23","2023-24")
+        year_combo.current(0)
+        year_combo.grid(row=1,column=1,padx=2,pady=10,sticky=W)
+
+
+        # Semester
+        semester_label=Label(current_course_frame,text="Semester",font=("times new roman",13,"bold"),bg="white")
+        semester_label.grid(row=1,column=2,padx=10,sticky=W)
+
+        semester_combo=ttk.Combobox(current_course_frame,font=("times new roman",13,"bold"),state="readonly",width=20)
+        semester_combo["values"]=("Select Semester","Semester-1","Semester-2","Semester-3","Semester-4")
+        semester_combo.current(0)
+        semester_combo.grid(row=1,column=3,padx=2,pady=10,sticky=W)
+
 
         # Right Label Frame
         Right_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="STUDENT DETAILS", font=("times new roman",12,"bold"))
         Right_frame.place(x=790,y=10,width=690,height=560)
+
+
+
 
 
 
