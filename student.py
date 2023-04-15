18571b840269df8d7853d19dfc11647f85fc740a
mohamedInfath <mohamedinfath99@gmail.com>
2023-04-15 22:27:18 +0530

    class student part done

--- student.py
+++ student.py
@@ -10,7 +10,7 @@ class Student:
         self.root.title("Student Attendance System")
 
 
-                # First Image
+        # First Image
         img=Image.open(r"D:\05. ICBT Notes\Student-Attendance-System\college_images\IcbtLogo.jpg")
         img=img.resize((500,150),Image.ANTIALIAS)
         self.photoimg=ImageTk.PhotoImage(img)
@@ -53,7 +53,7 @@ class Student:
 
         # Left Label Frame
         Left_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="STUDENT DETAILS", font=("times new roman",12,"bold"))
-        Left_frame.place(x=10,y=10,width=760,height=560)
+        Left_frame.place(x=10,y=2,width=760,height=570)
 
         img_left=Image.open(r"D:\05. ICBT Notes\Student-Attendance-System\college_images\student.jpg")
         img_left=img_left.resize((720,150),Image.ANTIALIAS)
@@ -65,7 +65,7 @@ class Student:
 
         # Current Course
         current_course_frame=LabelFrame(Left_frame,bd=2,bg="white",relief=RIDGE,text="CURRENT COURSE INFORMATION", font=("times new roman",12,"bold"))
-        current_course_frame.place(x=5,y=135,width=740,height=150)
+        current_course_frame.place(x=5,y=135,width=740,height=115)
 
 
         # Department
@@ -108,12 +108,146 @@ class Student:
         semester_combo.grid(row=1,column=3,padx=2,pady=10,sticky=W)
 
 
+        # Class Student Information
+        class_student_frame=LabelFrame(Left_frame,bd=2,bg="white",relief=RIDGE,text="CLASS STUDENT INFORMATION", font=("times new roman",12,"bold"))
+        class_student_frame.place(x=5,y=250,width=740,height=295)
+
+        #student id
+        studentId_label=Label(class_student_frame,text="Student ID:",font=("times new roman",13,"bold"),bg="white")
+        studentId_label.grid(row=0,column=0,padx=10,pady=5,sticky=W)
+
+        studentId_entry=ttk.Entry(class_student_frame,width=20,font=("times new roman",13,"bold"))
+        studentId_entry.grid(row=0,column=1,padx=10,pady=5,sticky=W)
+
+
+        #student name
+        studentName_label=Label(class_student_frame,text="Student Name:",font=("times new roman",13,"bold"),bg="white")
+        studentName_label.grid(row=0,column=2,padx=10,pady=5,sticky=W)
+
+        studentName_entry=ttk.Entry(class_student_frame,width=20,font=("times new roman",13,"bold"))
+        studentName_entry.grid(row=0,column=3,padx=10,pady=5,sticky=W)
+
+
+        #Class Division
+        class_div_label=Label(class_student_frame,text="Class Division:",font=("times new roman",13,"bold"),bg="white")
+        class_div_label.grid(row=1,column=0,padx=10,pady=5,sticky=W)
+
+        class_div_entry=ttk.Entry(class_student_frame,width=20,font=("times new roman",13,"bold"))
+        class_div_entry.grid(row=1,column=1,padx=10,pady=5,sticky=W)
+
+
+        #Roll No
+        roll_no_label=Label(class_student_frame,text="Roll No:",font=("times new roman",13,"bold"),bg="white")
+        roll_no_label.grid(row=1,column=2,padx=10,pady=5,sticky=W)
+
+        roll_no_entry=ttk.Entry(class_student_frame,width=20,font=("times new roman",13,"bold"))
+        roll_no_entry.grid(row=1,column=3,padx=10,pady=5,sticky=W)
+
+
+        # Gender
+        gender_label=Label(class_student_frame,text="Gender:",font=("times new roman",13,"bold"),bg="white")
+        gender_label.grid(row=2,column=0,padx=10,pady=5,sticky=W)
+
+        gender_entry=ttk.Entry(class_student_frame,width=20,font=("times new roman",13,"bold"))
+        gender_entry.grid(row=2,column=1,padx=10,pady=5,sticky=W)
+
+
+        # DOB
+        dob_label=Label(class_student_frame,text="DOB:",font=("times new roman",13,"bold"),bg="white")
+        dob_label.grid(row=2,column=2,padx=10,pady=5,sticky=W)
+
+        dob_entry=ttk.Entry(class_student_frame,width=20,font=("times new roman",13,"bold"))
+        dob_entry.grid(row=2,column=3,padx=10,pady=5,sticky=W)
+
+
+        # Email
+        email_label=Label(class_student_frame,text="Email:",font=("times new roman",13,"bold"),bg="white")
+        email_label.grid(row=3,column=0,padx=10,pady=5,sticky=W)
+
+        email_entry=ttk.Entry(class_student_frame,width=20,font=("times new roman",13,"bold"))
+        email_entry.grid(row=3,column=1,padx=10,pady=5,sticky=W)
+
+
+        # Phone No
+        phone_label=Label(class_student_frame,text="Phone No:",font=("times new roman",13,"bold"),bg="white")
+        phone_label.grid(row=3,column=2,padx=10,pady=5,sticky=W)
+
+        phone_entry=ttk.Entry(class_student_frame,width=20,font=("times new roman",13,"bold"))
+        phone_entry.grid(row=3,column=3,padx=10,pady=5,sticky=W)
+
+
+        # Address
+        address_label=Label(class_student_frame,text="Address:",font=("times new roman",13,"bold"),bg="white")
+        address_label.grid(row=4,column=0,padx=10,pady=5,sticky=W)
+
+        address_entry=ttk.Entry(class_student_frame,width=20,font=("times new roman",13,"bold"))
+        address_entry.grid(row=4,column=1,padx=10,pady=5,sticky=W)
+
+
+        # Teacher Name
+        teacher_label=Label(class_student_frame,text="Teacher Name:",font=("times new roman",13,"bold"),bg="white")
+        teacher_label.grid(row=4,column=2,padx=10,pady=5,sticky=W)
+
+        teacher_entry=ttk.Entry(class_student_frame,width=20,font=("times new roman",13,"bold"))
+        teacher_entry.grid(row=4,column=3,padx=10,pady=5,sticky=W)
+
+        # radio Buttons
+        radiobtn1=ttk.Radiobutton(class_student_frame,text="Take Photo Sample:",value="Yes")
+        radiobtn1.grid(row=5,column=0)
+
+        radiobtn2=ttk.Radiobutton(class_student_frame,text="No Photo Sample:",value="Yes")
+        radiobtn2.grid(row=5,column=1)
+
+        # button Frame
+        btn_frame=Frame(class_student_frame,bd=2,relief=RIDGE,bg="white")
+        btn_frame.place(x=5,y=210,width=720,height=35)
+
+
+        # save button
+        save_btn=Button(btn_frame,text="Save",width=17,font=("times new roman",13,"bold"),bg="blue",fg="white")
+        save_btn.grid(row=0,column=0,)
+
+
+        # update button
+        update_btn=Button(btn_frame,text="Update",width=17,font=("times new roman",13,"bold"),bg="blue",fg="white")
+        update_btn.grid(row=0,column=2)
+
+
+        # delete button
+        delete_btn=Button(btn_frame,text="Delete",width=17,font=("times new roman",13,"bold"),bg="blue",fg="white")
+        delete_btn.grid(row=0,column=3)
+
+
+        # reset button
+        reset_btn=Button(btn_frame,text="Reset",width=17,font=("times new roman",13,"bold"),bg="blue",fg="white")
+        reset_btn.grid(row=0,column=4)
+
+
+        # button Frame
+        btn_frame1=Frame(class_student_frame,bd=2,relief=RIDGE,bg="white")
+        btn_frame1.place(x=5,y=240,width=720,height=35)
+
+
+        # take photo sample button
+        take_photo_btn=Button(btn_frame1,text="Take Phot Sample",width=35,font=("times new roman",13,"bold"),bg="blue",fg="white")
+        take_photo_btn.grid(row=0,column=0)
+
+
+        # update photo sample button
+        update_photo_btn=Button(btn_frame1,text="Update Phot Sample",width=35,font=("times new roman",13,"bold"),bg="blue",fg="white")
+        update_photo_btn.grid(row=0,column=1)
+
+
+
+
+
+
+
+
+
         # Right Label Frame
         Right_frame=LabelFrame(main_frame,bd=2,bg="white",relief=RIDGE,text="STUDENT DETAILS", font=("times new roman",12,"bold"))
         Right_frame.place(x=790,y=10,width=690,height=560)
-
-
-
 
 
 
