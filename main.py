e8394bbcd408a6eb6b53f088d8bfda94829cdc6c
mohamedInfath <mohamedinfath99@gmail.com>
2023-05-12 00:41:42 +0530

    train data button link added

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from tkinter import*
 from tkinter import ttk
 from PIL import Image,ImageTk
+import os
 from student import Student
+from train import Train
+
 
 
 
@@ -13,7 +16,7 @@ class Student_Attendance_System:
 
 
         # First Image
-        img=Image.open(r"D:\01. Project\Student-Attendance-System\college_images\IcbtLogo.jpg")
+        img=Image.open(r"college_images\IcbtLogo.jpg")
         img=img.resize((500,150),Image.ANTIALIAS)
         self.photoimg=ImageTk.PhotoImage(img)
 
@@ -22,7 +25,7 @@ class Student_Attendance_System:
 
 
         # Second Image
-        img1=Image.open(r"D:\01. Project\Student-Attendance-System\college_images\facialrecognition.png")
+        img1=Image.open(r"college_images\facialrecognition.png")
         img1=img1.resize((500,150),Image.ANTIALIAS)
         self.photoimg1=ImageTk.PhotoImage(img1)
 
@@ -31,7 +34,7 @@ class Student_Attendance_System:
 
 
         # Third Image 
-        img2=Image.open(r"D:\01. Project\Student-Attendance-System\college_images\cardiff.jpg")
+        img2=Image.open(r"college_images\cardiff.jpg")
         img2=img2.resize((520,150),Image.ANTIALIAS)
         self.photoimg2=ImageTk.PhotoImage(img2)
 
@@ -40,7 +43,7 @@ class Student_Attendance_System:
 
 
         # Background Image
-        img3=Image.open(r"D:\01. Project\Student-Attendance-System\college_images\Teacher-with-students-1.jpg")
+        img3=Image.open(r"college_images\Teacher-with-students-1.jpg")
         img3=img3.resize((1530,710),Image.ANTIALIAS)
         self.photoimg3=ImageTk.PhotoImage(img3)
 
@@ -54,7 +57,7 @@ class Student_Attendance_System:
 
 
         # Button - STUDENT DETAILS
-        img4=Image.open(r"D:\01. Project\Student-Attendance-System\college_images\std.jpg")
+        img4=Image.open(r"college_images\std.jpg")
         img4=img4.resize((220,220),Image.ANTIALIAS)
         self.photoimg4=ImageTk.PhotoImage(img4)
 
@@ -66,7 +69,7 @@ class Student_Attendance_System:
 
 
         # Button - FACE DETECTOR
-        img5=Image.open(r"D:\01. Project\Student-Attendance-System\college_images\fad.jpg")
+        img5=Image.open(r"college_images\fad.jpg")
         img5=img5.resize((220,220),Image.ANTIALIAS)
         self.photoimg5=ImageTk.PhotoImage(img5)
 
@@ -78,7 +81,7 @@ class Student_Attendance_System:
 
 
         # Button - ATTENDENCE
-        img6=Image.open(r"D:\01. Project\Student-Attendance-System\college_images\attendant.jpg")
+        img6=Image.open(r"college_images\attendant.jpg")
         img6=img6.resize((220,220),Image.ANTIALIAS)
         self.photoimg6=ImageTk.PhotoImage(img6)
 
@@ -90,7 +93,7 @@ class Student_Attendance_System:
 
 
         # Button - REGISTER ADMIN
-        img7=Image.open(r"D:\01. Project\Student-Attendance-System\college_images\employee.jpg")
+        img7=Image.open(r"college_images\employee.jpg")
         img7=img7.resize((220,220),Image.ANTIALIAS)
         self.photoimg7=ImageTk.PhotoImage(img7)
 
@@ -102,31 +105,31 @@ class Student_Attendance_System:
 
 
         # Button - TRAIN DATA
-        img8=Image.open(r"D:\01. Project\Student-Attendance-System\college_images\traindata.jpg")
+        img8=Image.open(r"college_images\traindata.jpg")
         img8=img8.resize((220,220),Image.ANTIALIAS)
         self.photoimg8=ImageTk.PhotoImage(img8)
 
-        b1=Button(bg_img,image=self.photoimg8,cursor="hand2")
+        b1=Button(bg_img,image=self.photoimg8,cursor="hand2", command=self.train_data)
         b1.place(x=200,y=380,width=220,height=220)
 
-        b1_1=Button(bg_img,text="TRAIN DATA",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1=Button(bg_img,text="TRAIN DATA",cursor="hand2",command=self.train_data,font=("times new roman",15,"bold"),bg="darkblue",fg="white")
         b1_1.place(x=200,y=580,width=220,height=40)
 
 
         # Button - PHOTOS
-        img9=Image.open(r"D:\01. Project\Student-Attendance-System\college_images\photos.jpeg")
+        img9=Image.open(r"college_images\photos.jpeg")
         img9=img9.resize((220,220),Image.ANTIALIAS)
         self.photoimg9=ImageTk.PhotoImage(img9)
 
-        b1=Button(bg_img,image=self.photoimg9,cursor="hand2")
+        b1=Button(bg_img,image=self.photoimg9,cursor="hand2", command=self.open_img)
         b1.place(x=500,y=380,width=220,height=220)
 
-        b1_1=Button(bg_img,text="PHOTOS",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
+        b1_1=Button(bg_img,text="PHOTOS",cursor="hand2",command=self.open_img,font=("times new roman",15,"bold"),bg="darkblue",fg="white")
         b1_1.place(x=500,y=580,width=220,height=40)
 
 
         # Button - DEVELOPER
-        img10=Image.open(r"D:\01. Project\Student-Attendance-System\college_images\developer.jpg")
+        img10=Image.open(r"college_images\developer.jpg")
         img10=img10.resize((220,220),Image.ANTIALIAS)
         self.photoimg10=ImageTk.PhotoImage(img10)
 
@@ -138,7 +141,7 @@ class Student_Attendance_System:
 
 
         # Button - EXIT
-        img11=Image.open(r"D:\01. Project\Student-Attendance-System\college_images\exit.jpg")
+        img11=Image.open(r"college_images\exit.jpg")
         img11=img11.resize((220,220),Image.ANTIALIAS)
         self.photoimg11=ImageTk.PhotoImage(img11)
 
@@ -147,6 +150,10 @@ class Student_Attendance_System:
 
         b1_1=Button(bg_img,text="EXIT",cursor="hand2",font=("times new roman",15,"bold"),bg="darkblue",fg="white")
         b1_1.place(x=1100,y=580,width=220,height=40)
+    
+
+    def open_img(self):
+        os.startfile("data")
 
 
     # Button Function
@@ -154,6 +161,10 @@ class Student_Attendance_System:
         self.new_window=Toplevel(self.root)
         self.app=Student( self.new_window)
 
+
+    def train_data(self):
+        self.new_window=Toplevel(self.root)
+        self.app=Train( self.new_window)
 
 
 if __name__ == "__main__":
